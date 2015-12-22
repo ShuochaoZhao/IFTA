17ced23bd07f4b8115d9149c829a1a7dd6b90b62
szhao <shuochaozhao@gmail.com>
2015-12-22 16:32:41 -0500

    Filing Page __ Table Add & Delete Action

--- src/main/js/iftaFilingApp.js
+++ src/main/js/iftaFilingApp.js
@@ -4,25 +4,16 @@
 var iftaFilingApp = angular.module('iftaFilingApp', []);
 
 iftaFilingApp.controller('iftaFilingCtrl', function($scope) {
-    $scope.jurdctns = ['--Select--', 'AB', 'AL', 'AR', 'AZ', 'BC', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'IA', 'ID', 'IL',
-        'IN', 'KS', 'KY', 'LA', 'MA', 'MB', 'MD', 'ME', 'MN', 'MO', 'MS', 'MT', 'NB', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NL',
-        'NM', 'NS', 'NV', 'NY', 'OH', 'OK', 'ON', 'OR', 'PA', 'PE', 'QC', 'RI', 'SC', 'SD', 'SK', 'TN', 'TX', 'UT', 'VA',
+    $scope.tableRows;
+
+    $scope.jurdctns = [
+        'AB', 'AL', 'AR', 'AZ', 'BC', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'IA', 'ID', 'IL', 'IN', 'KS',
+        'KY', 'LA', 'MA', 'MB', 'MD', 'ME', 'MN', 'MO', 'MS', 'MT', 'NB', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NL', 'NM',
+        'NS', 'NV', 'NY', 'OH', 'OK', 'ON', 'OR', 'PA', 'PE', 'QC', 'RI', 'SC', 'SD', 'SK', 'TN', 'TX', 'UT', 'VA',
         'VT', 'WA', 'WI', 'WV', 'WY', 'Non-IFTA'
     ]
 
-    $scope.scheduleDatas = [{
-            D1:'AB',
-            taxEndDate: 3,
-            D4: 234,
-            D5: 423,
-            D7: 42342
-        }, {
-            D1: 'NY',
-            taxEndDate: 32,
-            D4: 2234,
-            D5: 4223,
-            D7: 423422
-        }];
+    $scope.scheduleDatas = [];
 
     $scope.addScheduleData = function(){
         var scheduleData = {
@@ -33,6 +24,7 @@ iftaFilingApp.controller('iftaFilingCtrl', function($scope) {
             D7: $scope.d7
         };
         $scope.scheduleDatas.push(scheduleData);
+        console.log($scope.scheduleDatas);
     };
 
     $scope.deleteScheduleData = function(index){
